b96a68fd6dafe11f643eb3cf5ddc2915f95604de
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2021-06-19 12:22:07 +0600

    module 7 react simple

---
+++ src/components/Product/Product.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    const {img,name, seller, price, stock} = props.product;
+    return (
+        <div className="product">
+            <div>
+                <img src={img} />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <br />
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button
+                    className="main-button"
+                    // onClick={props.handleAddProduct(props.product)}
+                    onClick={() => props.handleAddProduct(props.product)}
+                    >
+                    <FontAwesomeIcon icon={faShoppingCart} />
+                    add to cart
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
+import Product from '../Product/Product';
+import './Shop.css';
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) => {
+        // console.log('Product Added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+            <h1>This is product</h1>
+                    {
+                        products.map(pd => <Product
+                            handleAddProduct = {handleAddProduct}
+                            product={pd}
+                            ></Product>)
+                    }
+            </div>
+            <div className="cart-container">
+                {/* <h3>This is cart.</h3>
+                <h5>Order Summary: {cart.length}</h5> */}
+                <Cart cart={cart}></Cart>
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;
